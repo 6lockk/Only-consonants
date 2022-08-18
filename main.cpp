b145a92f9ec660db4e73b7e7883fb860ce279a28
Anirudh <109010287+6lockk@users.noreply.github.com>
2022-08-18 17:25:01 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,23 @@
+include <iostream.h>
+include <conio.h>
+include<stdio.h>
+include<string.h>
+void main()
+{
+    clrscr();
+    char str[30];
+    printf("Enter your String:");
+    scanf("%[^\n]",str);
+    int i;
+    printf("All the consonants in the string are:\n");
+    for(i=0;i<strlen(str);i++)
+    {
+    if(str[i]=='a' || str[i]=='A' || str[i]=='e'|| str[i]=='E' || str[i]=='i'
+       || str[i]=='I' || str[i]=='o' || str[i]=='O' || str[i]=='u' || str[i]=='U'|| str[i]==' ')
+    {
+     continue;
+    }
+    else
+        printf("%c ",str[i]);
+    }
+}
