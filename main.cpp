20c66619b2db00705d702642628f9d92436e8a66
Anirudh <109010287+6lockk@users.noreply.github.com>
2022-09-16 12:23:45 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
-include <iostream.h>
-include <conio.h>
-include<stdio.h>
-include<string.h>
+#include <iostream>
+#include <conio>
+#include <stdio>
+#include <string>
 void main()
 {
     clrscr();
